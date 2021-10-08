aa8a32594e6e628fd68bce48e8a29a77eed80542
daehee.kim <clonekim@gmail.com>
2021-10-08 12:51:26 +0900

    fix
    - websocket 연결 실패

--- agent/src/clj/agent/routes/api.clj
+++ agent/src/clj/agent/routes/api.clj
@@ -1,6 +1,6 @@
 (ns agent.routes.api
   (:require [clojure.tools.logging :as log]
-            [compojure.core :refer [defroutes context GET PUT POST DELETE]]
+            [compojure.core :refer [defroutes context GET PUT POST DELETE ANY]]
             [ring.util.http-response :refer [created ok not-found bad-request found set-cookie]]
             [slingshot.slingshot :refer [throw+ try+]]
             [org.httpkit.server :refer [send! with-channel on-close on-receive]]
@@ -22,7 +22,7 @@
 
 
 (defroutes api-routes
-  (GET "/socket" [] process-handler)
+  (ANY "/socket" [] process-handler)
   (POST "/trigger" [command buffer maxitem menuid]
     (case command
       "collect"
